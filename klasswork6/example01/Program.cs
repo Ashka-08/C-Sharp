6b7dd31d9b77e4a7120148ddb237f12e316c3832
Liubov <ashka-08@mail.ru>
2022-10-09 23:53:33 +0500

    lesson 6 add

---
+++ klasswork6/example01/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача/ Напишите программу, которая будет рекурсивно 
+выводить каждый элемент массива в консоль*/
+
+Console.Clear();
+
+int[] array = { 1, 12, 31, 4, 18 };
+PrintArray(array);
+
+void PrintArray(int[] array, int index = 0)
+{
+    if (index < array.Length)
+    {
+        Console.Write($"{array[index]} ");
+        PrintArray(array, ++index);
+    }
+}
+
+//if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+    
+// }
