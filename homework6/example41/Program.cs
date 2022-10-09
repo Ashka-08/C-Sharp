bdc21a10d4b4ebaa9969246a8b871223d7531e25
Liubov <ashka-08@mail.ru>
2022-10-09 23:55:08 +0500

    lesson 6 add

---
+++ homework6/example41/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры число M, 
+потом вводит M чисел ЧЕРЕЗ ENTER. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+5
+0
+7
+8
+-2
+-2 -> 2
+
+5
+1
+-7
+567
+89
+223-> 3*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+int[] array = new int[num];
+Function(array);
+Console.WriteLine($"Количество чисел больше 0 равно {CountDigits(array)}");
+
+void Function(int[] array, int index = 0)
+{
+    if (index < array.Length)
+    {
+        array[index] = int.Parse(Console.ReadLine()!);
+        Function(array, ++index);
+    }
+}
+
+int CountDigits(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            count ++;
+    }
+    return count;
+}
---
+++ klasswork6/example00/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача. Напишите программу, которая принимает на вход число, 
+и показывает количество цифр в числе
+456 -> 3
+78 -> 2*/
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Количество цифр в числе {Digits(number)}");
+
+/*int Digits(int number, int count = 1)
+{
+    if (number / 10 == 0)
+        return count;
+    else 
+    {
+        return count + Digits(number / 10);
+    }
+}*/
+
+int Digits(int number)
+{
+    if (number / 10 < 1)
+        return 1;
+    else
+        return 1 + Digits(number/10);
+}
